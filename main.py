0a74dd1e80b66bf47f96dedb9f8459c78b50493e
Kloowy <93261232+Kloowy@users.noreply.github.com>
2022-06-18 19:55:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def divisorGen(n):
+def divisorsGen(n):
     list=[]
     for i in range(1,n+1):
         if n%i==0:list.append(i)
@@ -6,15 +6,21 @@ def divisorGen(n):
 def text2bf(text):
     bf_code=""
     for sym in text:
-        num=ord(sym);code_block=""
-        for i in range(255):
-            divisors=divisorGen(256-i)
-            for divider in divisors:
-                coeff=int((255/divider)//num+(((255/divider)//num)-1)%2)
-                if 255//coeff-num>=0:sign="-"
-                else:sign="+"
-                if len("-"*i+"[>"+"+"*divider+"<"+str("-"*coeff)+"]>"+sign*abs(255//coeff-num)+".>")<len(code_block) or code_block=="":
-                    code_block="-[>"+"+"*divider+"<"+str("-"*coeff)+"]>"+sign*abs(255//coeff-num)+".>"
+        num=ord(sym);
+        if len("+"*num)>len("-"*(256-num)):code_block="+"*num+".>"
+        else:code_block="-"*(256-num)+".>"
+        for fs in ["-", "+"]:
+            for i in range(1, 255):
+                divisors=divisorsGen(256-i)
+                for divider in divisors:
+                    coeff=int((255/divider)//num+(((255/divider)//num)-1)%2)
+                    if 255//coeff-num>=0:sign="-"
+                    else:sign="+"
+                    if len("+"*divider)<=len("-"*(256-divider)):seg="+"*divider
+                    else:seg="-"*(256-divider)
+                    if len("+"*coeff)<=len("-"*(256-coeff)):seg2="+"*coeff
+                    else:seg2="-"*(256-coeff)
+                    if len(fs*i+"[>"+seg+"<"+seg2+"]>"+sign*abs(255//coeff-num)+".>")<len(code_block):code_block=fs*i+"[>"+"+"*divider+"<"+str("-"*coeff)+"]>"+sign*abs(255//coeff-num)+".>"
         bf_code+=code_block
     return bf_code
 if __name__=='__main__':
